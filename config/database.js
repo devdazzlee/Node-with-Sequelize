b6df75aaa811a9c75966de47d0208678ec788418
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2024-09-28 00:06:32 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+
 dotenv.config();
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } = process.env;
 
-// Create Sequelize instance
+
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,      // The host is 'localhost'
   port: DB_PORT,      // The port is '3306'
   dialect: DB_DIALECT // 'mysql' in this case
 });
 
-// Test the database connection
+
 (async () => {
   try {
     await sequelize.authenticate();
